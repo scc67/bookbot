0fc8f1d3ac325bbc1b8f7d10ec1905ab9c48a8ec
Steve <31158584+scc67@users.noreply.github.com>
2025-02-06 00:05:23 +0100

    a description of your changes here

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     
-    print(file_contents)
+
+    def count_words(tekst):
+        return len(tekst.split())
+
+    def count_characters(tekst):
+        letter_count = {}
+        for char in tekst:
+            if char.lower() in letter_count:
+                letter_count[char.lower()] += 1
+            else:
+                letter_count[char.lower()] = 1
+        
+        return letter_count
+    
+    
+    count_dict = count_characters(file_contents)
+    count_list = []
+    for letter in count_dict:
+        dict = {}
+        dict["name"] = letter
+        dict["num"] = count_dict[letter]
+        count_list.append(dict)
+        
+    def sort_on(dict):
+        return dict["num"]
+    
+    count_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {f.name} ---")
+    print(f"{count_words(file_contents)} word found in the document")
+    print()
+    for k in count_dict:
+        if k.isalpha():
+            print(f"The '{k}' character was found {count_dict[k]} times")
+    print("--- End report ---")
 
 main()
